87d142d1bba0148b3d0f7bfdbd841e67d08e2520
Derek Verduijn <derek.verduijn@outlook.com>
2025-06-01 19:29:39 +0200

    Start SvelteKitting

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,4 +1,6 @@
-// Write typescript code here
+// For a good TypeScript and Javascript tutorial: use these:
+// JavaScript: https://www.w3schools.com/js/DEFAULT.asp
+// TypeScript: https://learnxinyminutes.com/typescript/
 
 //Example: function that generates a random number;
 export const randomInt = (min: number, max: number) => {
